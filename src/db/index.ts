3e86444526b0b8a6039cb30e200d6d4b30db5cfe
mhdfahrurozi <mhdfahrurozi5@gmail.com>
2026-06-22 21:14:21 +0700

    update index.ts db

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,12 +1,12 @@
-import { drizzle } from "drizzle-orm/mysql2";
+import { drizzle, MySql2Database } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 import * as schema from "./schema";
 
-let _db: ReturnType<typeof drizzle> | null = null;
+let _db: MySql2Database<typeof schema> | null = null;
 
 export async function getDb() {
   if (!_db) {
-    const connection = await mysql.createConnection({
+    const pool = mysql.createPool({
       host: process.env.DB_HOST || "localhost",
       port: Number(process.env.DB_PORT) || 3306,
       user: process.env.DB_USER || "root",
@@ -14,7 +14,7 @@ export async function getDb() {
       database: process.env.DB_NAME || "first_project",
     });
 
-    _db = drizzle(connection, { schema, mode: "default" });
+    _db = drizzle(pool, { schema, mode: "default" });
   }
-  return _db;
+  return _db!;
 }
